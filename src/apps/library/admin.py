f54cd530ebab794cc201f134eda7a20be728deec
ArtemSerdechnyi <artem.serdechnyy@gmail.com>
2024-02-16 01:56:42 +0200

    add admin for library app

--- src/apps/library/admin.py
+++ src/apps/library/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Country, Author, Genre, Book, UserBookInstance
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    show_full_result_count = False
+
+    list_display = ['name']
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    show_full_result_count = False
+
+    list_display = ['full_name', 'country']
+    search_fields = ['full_name']
+    list_filter = ['country']
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    show_full_result_count = False
+
+    list_display = ['name']
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    show_full_result_count = False
+
+    list_display = ['title', 'year_of_publication']
+    filter_horizontal = ['authors', 'genre']
+    search_fields = ['title']
+
+
+
+
+@admin.register(UserBookInstance)
+class UserBookInstanceAdmin(admin.ModelAdmin):
+    show_full_result_count = False
+
+    list_display = ['user', 'book', 'is_read']
+    list_filter = ['user', 'is_read']
