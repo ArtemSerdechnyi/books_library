8611d7840cf9b6a239fbc107c8d1152886661040
artem <artem.serdechnyy@gmail.com>
2024-02-27 21:41:46 +0200

    add add_book_to_user_library view

--- src/apps/library/urls.py
+++ src/apps/library/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path('', views.home_page_view, name='index'),
     path(f'{app_name}/book/<slug:slug>/', views.BookView.as_view(), name='book'),
     path(f'{app_name}/', views.Library.as_view(), name='library'),
-    path('add_book/', views.AddBook.as_view(), name='add_book'),
-    path('library_search/', views.LibrarySearch.as_view(), name='library_search'),
+    path(f'{app_name}/add_book/', views.AddBook.as_view(), name='add_book'),
+    path(f'{app_name}/library_search/', views.LibrarySearch.as_view(), name='library_search'),
+    path('user_books/', views.UserLibrary.as_view(), name='user_books'),
+    path('add_book_to_user_library/<int:id>/', views.add_book_to_user_library, name='add_book_to_user_library'),
+
 ]
--- src/apps/library/views.py
+++ src/apps/library/views.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.handlers.wsgi import WSGIRequest
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import FormView, ListView, DetailView
 
 from .forms import BookForm
-from .models import Book
+from .models import Book, UserBookInstance
 from .utils import SearchBookMixin
 
 
@@ -11,21 +12,18 @@ def home_page_view(request: WSGIRequest):
     return render(request, 'library/index.html')
 
 
+def add_book_to_user_library(request: WSGIRequest, id):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            book = get_object_or_404(Book, id=id)
+            UserBookInstance.objects.create(book=book, user=request.user)
+    return render(request, 'library/index.html')
+
+
 class BookView(DetailView):
     model = Book
     template_name = 'library/book.html'
     context_object_name = 'book'
-
-
-class Library(SearchBookMixin, ListView):
-    model = Book
-    template_name = 'library/library.html'
-    context_object_name = 'books'
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        queryset = self.search_book(queryset)
-        return queryset
 
 
 class LibrarySearch(SearchBookMixin, ListView):
@@ -37,6 +35,30 @@ class LibrarySearch(SearchBookMixin, ListView):
         queryset = super().get_queryset()
         queryset = self.search_book(queryset)
         return queryset
+
+
+class Library(SearchBookMixin, ListView):
+    model = Book
+    template_name = 'library/library.html'
+    context_object_name = 'books'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = self.search_book(queryset)
+        if self.request.user.is_authenticated:
+            user_books = self.request.user.books.all()
+        return queryset
+
+
+class UserLibrary(LoginRequiredMixin, ListView):
+    model = UserBookInstance
+    template_name = 'library/user_library.html'
+    context_object_name = 'user_books'
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            user_books = self.request.user.books.all()
+        return user_books.all()
 
 
 class AddBook(FormView):
